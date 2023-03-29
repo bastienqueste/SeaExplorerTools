42cdd995bb32a1b9ba098743ae4ad5794fb7f077
Callum Rollo <c.rollo@outlook.com>
2023-03-29 11:25:16 +0200

    add basic integration test to ensure results do not change

---
+++ tests/test_adcp_processing.py
@@ -0,0 +1,62 @@
+import xarray as xr
+import numpy as np
+from pathlib import Path
+import sys
+module_dir = Path(__file__).parent.parent.absolute()
+sys.path.append(str(module_dir))
+from seaexplorertools import process_adcp
+
+
+def test_processing():
+    adcp_path = 'ADCP_refactoring_test_files/sea045_M44.ad2cp.00000*.nc'
+    glider_pqt_path = 'ADCP_refactoring_test_files/Skag_test.pqt'
+    options = {
+        'debug': True,
+        'correctADCPHeading': True,
+        'ADCP_discardFirstBins': 0,
+        'ADCP_correlationThreshold': 70,
+        'ADCP_amplitudeThreshold': 75,
+        'ADCP_velocityThreshold': 0.8,
+        'correctXshear': False,
+        'correctYshear': False,
+        'correctZshear': False,
+        'correctZZshear': False,
+        'top_mounted': True
+    }
+    ADCP, data, ADCP_settings = process_adcp.load_adcp_glider_data(adcp_path, glider_pqt_path)
+    ADCP = process_adcp.remapADCPdepth(ADCP, options)
+    ADCP = process_adcp.correct_heading(ADCP, data, options)
+    ADCP = process_adcp.soundspeed_correction(ADCP)
+    ADCP = process_adcp.remove_outliers(ADCP, options)
+    process_adcp.plot_data_density(ADCP, ADCP_settings)
+    ADCP = process_adcp.correct_shear(ADCP, options)
+    ADCP = process_adcp.correct_backscatter(ADCP, data)
+    ADCP = process_adcp.regridADCPdata(ADCP, ADCP_settings, options)
+    ADCP = process_adcp.calcXYZfrom3beam(ADCP, options)
+    ADCP = process_adcp.calcENUfromXYZ(ADCP, data, options)
+    process_adcp.verify_calcENUfromXYZ(ADCP)
+    data = process_adcp.get_DAC(ADCP, data)
+    dE, dN, dT = process_adcp.getSurfaceDrift(data)
+    ADCP = process_adcp.bottom_track(ADCP, adcp_path, options)
+    out, xaxis, yaxis, taxis, days = process_adcp.verify_bottom_track(ADCP, data, dE, dN, dT)
+    out = process_adcp.grid_data(ADCP, data, out, xaxis, yaxis)
+    process_adcp.plot_bias(out, yaxis, taxis, days)
+
+    profiles = np.arange(out["Pressure"].shape[1])
+    depth_bins = np.arange(out["Pressure"].shape[0])
+
+    ds_dict = {}
+    for key, val in out.items():
+        ds_dict[key] = (("depth_bin", "profile_num",), val)
+    coords_dict = {"profile_num": (("profile_num"), profiles),
+                   "depth_bin": (("depth_bin"), depth_bins)
+                   }
+    ds = xr.Dataset(data_vars=ds_dict, coords=coords_dict)
+    ds_min = ds[['Sh_E', 'Sh_N', 'Sh_U', 'ADCP_E', 'ADCP_N']]
+    ds_min_test = xr.open_dataset("tests/test_files/ds_out_min.nc")
+    for var in list(ds_min):
+        assert np.allclose(ds_min[var], ds_min_test[var], equal_nan=True, atol=1e-7, rtol=1e-3)
+
+
+if __name__ == '__main__':
+    test_processing()
